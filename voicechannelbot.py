547c71959520d721a06bfeae6e280a5abaddfe96
Defelo <elodef42@gmail.com>
2020-03-15 22:47:32 +0100

    used discord.py commands and added bot mention prefix

--- voicechannelbot.py
+++ voicechannelbot.py
@@ -1,9 +1,8 @@
 import os
-import re
-from typing import Optional
+from typing import Optional, Iterable
 
-from discord import Client, Member, VoiceState, Message, Role, DMChannel, Guild, VoiceChannel
-from discord.utils import get
+from discord import Member, VoiceState, Message, Role, Guild, VoiceChannel
+from discord.ext.commands import Bot, Context, check, CommandError
 
 from database import db, run_in_thread
 from models.authorizes_roles import AuthorizedRoles
@@ -15,211 +14,227 @@ db.create_tables()
 DEFAULT_PREFIX = "!"
 
 
-class Bot(Client):
-    async def on_ready(self):
-        print(f"Logged in as {self.user}")
+def get_prefix(guild: Guild) -> str:
+    if (server := db.get(Server, guild.id)) is None:
+        server = Server.create(guild.id, DEFAULT_PREFIX)
 
-    @staticmethod
-    async def check_access(member: Member) -> int:
-        if member.id == 370876111992913922:
-            return 3
+    return server.prefix
 
-        if member.guild_permissions.administrator:
-            return 2
 
-        roles = set(role.id for role in member.roles)
-        for auth in await run_in_thread(db.query, AuthorizedRoles, server=member.guild.id):
-            if auth.role in roles:
-                return 1
-        return 0
+def set_prefix(guild: Guild, new_prefix: str) -> str:
+    if (server := db.get(Server, guild.id)) is None:
+        server = Server.create(guild.id, new_prefix)
+    else:
+        server.prefix = new_prefix
 
-    @staticmethod
-    def get_prefix(guild: Guild) -> str:
-        if (server := db.get(Server, guild.id)) is None:
-            server = Server.create(guild.id, DEFAULT_PREFIX)
+    return server.prefix
 
-        return server.prefix
 
-    @staticmethod
-    def set_prefix(guild: Guild, prefix: str) -> str:
-        if (server := db.get(Server, guild.id)) is None:
-            server = Server.create(guild.id, prefix)
-        else:
-            server.prefix = prefix
+async def fetch_prefix(_, message: Message) -> Iterable[str]:
+    if message.guild is None:
+        return ""
+    return await run_in_thread(get_prefix, message.guild), f"<@!{bot.user.id}> "
 
-        return server.prefix
 
-    async def on_message(self, message: Message):
-        if message.author == self.user or isinstance(message.channel, DMChannel):
-            return
+bot = Bot(command_prefix=fetch_prefix)
 
-        guild: Guild = message.guild
 
-        prefix: str = await run_in_thread(Bot.get_prefix, guild)
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
 
-        if not message.content.startswith(prefix):
-            return
 
-        if not (permission_level := await Bot.check_access(message.author)):
-            await message.channel.send(f"permission denied {message.author.mention}")
-            return
+async def check_access(member: Member) -> int:
+    if member.id == 370876111992913922:
+        return 3
 
-        line = message.content[len(prefix) :]
-        cmd, *args = line.split()
+    if member.guild_permissions.administrator:
+        return 2
 
-        if cmd == "prefix":
-            if len(args) != 1:
-                await message.channel.send(f"usage: {prefix}prefix <new-prefix>")
-                return
+    roles = set(role.id for role in member.roles)
+    for authorization in await run_in_thread(db.query, AuthorizedRoles, server=member.guild.id):
+        if authorization.role in roles:
+            return 1
+    return 0
 
-            new_prefix = args[0]
-            if not 0 < len(new_prefix) <= 16:
-                await message.channel.send("length of prefix must be between 1 and 16")
-                return
 
-            await run_in_thread(Bot.set_prefix, guild, new_prefix)
-            await message.channel.send("prefix has been updated")
-        elif cmd == "auth":
-            if permission_level < 2:
-                await message.channel.send(f"permission denied {message.author.mention}")
-                return
-            if not re.match(r"^auth (list|(add|del) (\d+))$", line):
-                await message.channel.send(f"usage: {prefix}auth list|add|del [<role-id>]")
-                return
+def permission_level(level: int):
+    @check
+    async def admin_only(ctx: Context):
+        return await check_access(ctx.author) >= level
 
-            if args[0] == "list":
-                roles = []
-                for auth_role in await run_in_thread(db.query, AuthorizedRoles, server=guild.id):
-                    if (role := guild.get_role(auth_role.role)) is not None:
-                        roles.append(f" - `@{role}` (`{role.id}`)")
-                if roles:
-                    await message.channel.send(
-                        "the following roles are authorized to control this bot:\n" + "\n".join(roles)
-                    )
-                else:
-                    await message.channel.send("only administrators can control this bot")
-            elif args[0] == "add":
-                role: Optional[Role] = guild.get_role(int(args[1]))
-                if role is None:
-                    await message.channel.send("this role does not exist")
-                    return
+    return admin_only
 
-                auth: Optional[AuthorizedRoles] = await run_in_thread(
-                    db.first, AuthorizedRoles, server=guild.id, role=role.id
+
+@bot.command()
+@permission_level(1)
+async def prefix(ctx: Context, *, new_prefix: str):
+    """
+    change the bot prefix
+    """
+
+    if not 0 < len(new_prefix) <= 16:
+        await ctx.send("length of prefix must be between 1 and 16")
+        return
+
+    await run_in_thread(set_prefix, ctx.guild, new_prefix)
+    await ctx.send("prefix has been updated :white_check_mark:")
+
+
+@bot.group()
+@permission_level(2)
+async def auth(ctx: Context):
+    """
+    manage roles authorized to control this bot
+    """
+
+    if ctx.invoked_subcommand is None:
+        await ctx.send_help("auth")
+
+
+@auth.command(name="list")
+async def auth_list(ctx: Context):
+    """
+    list authorized roles
+    """
+
+    roles = []
+    for auth_role in await run_in_thread(db.query, AuthorizedRoles, server=ctx.guild.id):
+        if (role := ctx.guild.get_role(auth_role.role)) is not None:
+            roles.append(f" - `@{role}` (`{role.id}`)")
+    if roles:
+        await ctx.send("the following roles are authorized to control this bot:\n" + "\n".join(roles))
+    else:
+        await ctx.send("only administrators can control this bot")
+
+
+@auth.command(name="add")
+async def auth_add(ctx: Context, *, role: Role):
+    """
+    authorize role to control this bot
+    """
+
+    authorization: Optional[AuthorizedRoles] = await run_in_thread(
+        db.first, AuthorizedRoles, server=ctx.guild.id, role=role.id
+    )
+    if authorization is not None:
+        await ctx.send(f"role `@{role}` is already authorized")
+        return
+
+    await run_in_thread(AuthorizedRoles.create, ctx.guild.id, role.id)
+    await ctx.send(f"role `@{role}` has been authorized to control this bot")
+
+
+@auth.command(name="del")
+async def auth_del(ctx: Context, *, role: Role):
+    """
+    unauthorize role to control this bot
+    """
+
+    authorization: Optional[AuthorizedRoles] = await run_in_thread(
+        db.first, AuthorizedRoles, server=ctx.guild.id, role=role.id
+    )
+    if auth is None:
+        await ctx.send(f"role `@{role}` is not authorized")
+        return
+
+    await run_in_thread(db.delete, authorization)
+    await ctx.send(f"role `@{role}` has been unauthorized to control this bot")
+
+
+@bot.command(name="list")
+@permission_level(1)
+async def list_links(ctx: Context):
+    """
+    list all links between voice channels and roles
+    """
+
+    out = []
+    for link in await run_in_thread(db.query, RoleVoiceLink, server=ctx.guild.id):
+        if (role := ctx.guild.get_role(link.role)) is None:
+            continue
+        if (voice := ctx.guild.get_channel(link.voice_channel)) is None:
+            continue
+
+        out.append(f"`{voice}` (`{voice.id}`) -> `@{role}` (`{role.id}`)")
+
+    await ctx.send("\n".join(out) or "no links created")
+
+
+@bot.command(name="link")
+@permission_level(1)
+async def create_link(ctx: Context, voice_channel: VoiceChannel, *, role: Role):
+    """
+    link a voice channel with a role
+    """
+
+    if (
+        await run_in_thread(db.first, RoleVoiceLink, server=ctx.guild.id, role=role.id, voice_channel=voice_channel.id)
+        is not None
+    ):
+        await ctx.send("link already exists")
+        return
+
+    await run_in_thread(RoleVoiceLink.create, ctx.guild.id, role.id, voice_channel.id)
+    await ctx.send(f"link has been created between voice channel `{voice_channel}` and role `@{role}`")
+
+
+@bot.command(name="unlink")
+@permission_level(1)
+async def remove_link(ctx: Context, voice_channel: VoiceChannel, *, role: Role):
+    """
+    delete the link between a voice channel and a role
+    """
+
+    if (
+        link := await run_in_thread(
+            db.first, RoleVoiceLink, server=ctx.guild.id, role=role.id, voice_channel=voice_channel.id
+        )
+    ) is None:
+        await ctx.send("link does not exist")
+        return
+
+    await run_in_thread(db.delete, link)
+    await ctx.send(f"link has been deleted")
+
+
+@bot.event
+async def on_voice_state_update(member: Member, before: VoiceState, after: VoiceState):
+    if before.channel == after.channel:
+        return
+
+    guild: Guild = member.guild
+    if before.channel is not None:
+        await member.remove_roles(
+            *(
+                role
+                for link in await run_in_thread(
+                    db.query, RoleVoiceLink, server=guild.id, voice_channel=before.channel.id
                 )
-                if auth is not None:
-                    await message.channel.send(f"role `@{role}` is already authorized")
-                    return
-
-                await run_in_thread(AuthorizedRoles.create, guild.id, role.id)
-                await message.channel.send(f"role `@{role}` has been authorized to control this bot")
-            elif args[0] == "del":
-                role_id = int(args[1])
-                auth: Optional[AuthorizedRoles] = await run_in_thread(
-                    db.first, AuthorizedRoles, server=guild.id, role=role_id
-                )
-
-                if auth is None:
-                    if (role := guild.get_role(role_id)) is not None:
-                        await message.channel.send(f"role `@{role}` is not authorized")
-                    else:
-                        await message.channel.send("role is not authorized and does not exist")
-                    return
-
-                await run_in_thread(db.delete, auth)
-                if (role := guild.get_role(role_id)) is not None:
-                    await message.channel.send(f"role `@{role}` has been unauthorized to control this bot")
-                else:
-                    await message.channel.send("role has been unauthorized to control this bot")
-        elif cmd == "list":
-            out = [
-                f"`{voice}` (`{voice.id}`) -> `@{role}` (`{role.id}`)"
-                for link in await run_in_thread(db.query, RoleVoiceLink, server=guild.id)
                 if (role := guild.get_role(link.role)) is not None
-                and (voice := guild.get_channel(link.voice_channel)) is not None
-            ]
-            await message.channel.send("\n".join(out) or "no links created")
-        elif cmd == "link":
-            if not re.match(r"^link (\d+) ((\d+)|([a-zA-Z0-9_\-]+))$", line):
-                await message.channel.send(f"usage: {prefix}link <voice-channel-id> <role-id>|<role-name>")
-                return
-
-            voice_id = int(args[0])
-
-            if args[1].isnumeric():
-                if (role := guild.get_role(int(args[1]))) is None:
-                    await message.channel.send("role does not exist")
-                    return
-            else:
-                if (role := get(guild.roles, name=args[1])) is None:
-                    role = await guild.create_role(name=args[1], mentionable=True)
-
-            if (
-                await run_in_thread(db.first, RoleVoiceLink, server=guild.id, role=role.id, voice_channel=voice_id)
-                is not None
-            ):
-                await message.channel.send("link already exists")
-                return
-
-            if (voice := guild.get_channel(voice_id)) is None or not isinstance(voice, VoiceChannel):
-                await message.channel.send("voice channel does not exist")
-                return
-
-            await run_in_thread(RoleVoiceLink.create, guild.id, role.id, voice_id)
-            await message.channel.send(f"link has been created between voice channel `{voice}` and role `@{role}`")
-        elif cmd == "unlink":
-            if not re.match(r"^unlink (\d+) ((\d+)|([a-zA-Z0-9_\-]+))$", line):
-                await message.channel.send(f"usage: {prefix}unlink <voice-channel-id> <role-id>|<role-name>")
-                return
-
-            voice_id = int(args[0])
-
-            if args[1].isnumeric():
-                role_id = int(args[1])
-            else:
-                if (role := get(guild.roles, name=args[1])) is None:
-                    await message.channel.send("role does not exist")
-                    return
-                role_id = role.id
-
-            if (
-                link := await run_in_thread(
-                    db.first, RoleVoiceLink, server=guild.id, role=role_id, voice_channel=voice_id
-                )
-            ) is None:
-                await message.channel.send("link does not exists")
-                return
-
-            await run_in_thread(db.delete, link)
-            await message.channel.send(f"link has been deleted")
-        else:
-            await message.channel.send("usage: prefix|auth|list|link|unlink [<arguments>]")
-
-    async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
-        if before.channel == after.channel:
-            return
-
-        guild: Guild = member.guild
-        if before.channel is not None:
-            await member.remove_roles(
-                *(
-                    role
-                    for link in await run_in_thread(
-                        db.query, RoleVoiceLink, server=guild.id, voice_channel=before.channel.id
-                    )
-                    if (role := guild.get_role(link.role)) is not None
-                )
             )
-        if after.channel is not None:
-            await member.add_roles(
-                *(
-                    role
-                    for link in await run_in_thread(
-                        db.query, RoleVoiceLink, server=guild.id, voice_channel=after.channel.id
-                    )
-                    if (role := guild.get_role(link.role)) is not None
+        )
+    if after.channel is not None:
+        await member.add_roles(
+            *(
+                role
+                for link in await run_in_thread(
+                    db.query, RoleVoiceLink, server=guild.id, voice_channel=after.channel.id
                 )
+                if (role := guild.get_role(link.role)) is not None
             )
+        )
 
 
-Bot().run(os.environ["TOKEN"])
+@bot.event
+async def on_command_error(ctx: Context, error: CommandError):
+    await ctx.send(f":x: Error: {error}")
+
+
+@bot.event
+async def on_message(message: Message):
+    if message.guild is not None:
+        await bot.process_commands(message)
+
+
+bot.run(os.environ["TOKEN"])
