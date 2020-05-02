ce3cfe78bdac19422f26d1576056ca17ae3893d9
Defelo <elodef42@gmail.com>
2020-05-02 23:39:37 +0200

    moved rule command strings to yaml file (#34)

--- cogs/rules.py
+++ cogs/rules.py
@@ -7,6 +7,7 @@ from discord import TextChannel, Message, File, Forbidden, Permissions, Embed, C
 from discord.ext import commands
 from discord.ext.commands import Cog, Bot, guild_only, Context, CommandError
 
+from translations import translations
 from util import permission_level
 
 
@@ -24,11 +25,11 @@ class RulesCog(Cog, name="Rule Commands"):
         return msg.content, files
 
     async def read_embed(self, channel: TextChannel, author: Member) -> Embed:
-        await channel.send("Send me the title of the embed!")
+        await channel.send(translations.send_embed_title)
         title: str = (
             await self.bot.wait_for("message", check=lambda m: m.channel == channel and m.author == author)
         ).content
-        await channel.send("Ok, now send me the content of the embed!")
+        await channel.send(translations.send_embed_content)
         content: str = (
             await self.bot.wait_for("message", check=lambda m: m.channel == channel and m.author == author)
         ).content
@@ -52,18 +53,16 @@ class RulesCog(Cog, name="Rule Commands"):
         """
 
         if not channel.permissions_for(channel.guild.me).send_messages:
-            raise CommandError(
-                "Message could not be sent because I don't have `send_messages` permission in this channel."
-            )
+            raise CommandError(translations.could_not_send_message)
 
-        await ctx.send("Now send me the message!")
+        await ctx.send(translations.send_message)
         content, files = await self.read_normal_message(ctx.channel, ctx.author)
         try:
             await channel.send(content=content, files=files)
         except (HTTPException, Forbidden):
-            raise CommandError("Message could not be sent.")
+            raise CommandError(translations.msg_could_not_be_sent)
         else:
-            await ctx.send("Message has been sent successfully.")
+            await ctx.send(translations.msg_sent)
 
     @send.command(name="embed", aliases=["e"])
     async def send_embed(self, ctx: Context, channel: TextChannel, color: Optional[Union[Color, str]] = None):
@@ -73,19 +72,15 @@ class RulesCog(Cog, name="Rule Commands"):
 
         if isinstance(color, str):
             if not re.match(r"^[0-9a-fA-F]{6}$", color):
-                raise CommandError("Invalid color")
+                raise CommandError(translations.invalid_color)
             else:
                 color = int(color, 16)
 
         permissions: Permissions = channel.permissions_for(channel.guild.me)
         if not permissions.send_messages:
-            raise CommandError(
-                "Message could not be sent because I don't have `send_messages` permission in this channel."
-            )
+            raise CommandError(translations.could_not_send_message)
         elif not permissions.embed_links:
-            raise CommandError(
-                "Message could not be sent because I don't have `embed_links` permission in this channel."
-            )
+            raise CommandError(translations.could_not_send_embed)
 
         embed = await self.read_embed(ctx.channel, ctx.author)
         if color is not None:
@@ -93,9 +88,9 @@ class RulesCog(Cog, name="Rule Commands"):
         try:
             await channel.send(embed=embed)
         except (HTTPException, Forbidden):
-            raise CommandError("Message could not be sent.")
+            raise CommandError(translations.msg_could_not_be_sent)
         else:
-            await ctx.send("Message has been sent successfully.")
+            await ctx.send(translations.msg_sent)
 
     @commands.group(name="edit")
     @permission_level(1)
@@ -115,12 +110,12 @@ class RulesCog(Cog, name="Rule Commands"):
         """
 
         if message.author != self.bot.user:
-            raise CommandError("This message cannot be edited because the bot is not the author of the message.")
+            raise CommandError(translations.could_not_edit)
 
-        await ctx.send("Now send me the new message!")
+        await ctx.send(translations.send_new_message)
         content, files = await self.read_normal_message(ctx.channel, ctx.author)
         await message.edit(content=content, files=files, embed=None)
-        await ctx.send("Message has been edited successfully.")
+        await ctx.send(translations.msg_edited)
 
     @edit.command(name="embed", aliases=["e"])
     async def edit_embed(self, ctx: Context, message: Message, color: Optional[Union[Color, str]] = None):
@@ -129,11 +124,11 @@ class RulesCog(Cog, name="Rule Commands"):
         """
 
         if message.author != self.bot.user:
-            raise CommandError("This message cannot be edited because the bot is not the author of the message.")
+            raise CommandError(translations.could_not_edit)
 
         if isinstance(color, str):
             if not re.match(r"^[0-9a-fA-F]{6}$", color):
-                raise CommandError("Invalid color")
+                raise CommandError(translations.invalid_color)
             else:
                 color = int(color, 16)
 
@@ -141,7 +136,7 @@ class RulesCog(Cog, name="Rule Commands"):
         if color is not None:
             embed.colour = color
         await message.edit(content=None, files=[], embed=embed)
-        await ctx.send("Message has been edited successfully.")
+        await ctx.send(translations.msg_edited)
 
     @commands.command(name="delete")
     @permission_level(1)
@@ -152,14 +147,12 @@ class RulesCog(Cog, name="Rule Commands"):
         """
 
         if message.guild is None:
-            raise CommandError("Private messages cannot be deleted.")
+            raise CommandError(translations.cannot_delete_dm)
 
         channel: TextChannel = message.channel
         permissions: Permissions = channel.permissions_for(message.guild.me)
         if message.author != self.bot.user and not permissions.manage_messages:
-            raise CommandError(
-                f"Message could not be deleted because I don't have `manage_messages` permission in {channel.mention}."
-            )
+            raise CommandError(translations.could_not_delete)
 
         await message.delete()
-        await ctx.send("Message has been deleted successfully.")
+        await ctx.send(translations.msg_deleted)
