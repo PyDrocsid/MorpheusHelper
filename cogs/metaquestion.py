65f362251e5f004846b5ac4cb34ced35fe82fc2e
Defelo <elodef42@gmail.com>
2020-05-02 22:55:32 +0200

    moved metaquestion strings to yaml file (#34)

--- cogs/metaquestion.py
+++ cogs/metaquestion.py
@@ -6,44 +6,24 @@ from discord.ext.commands import Cog, Bot, Context, guild_only
 
 from database import run_in_thread, db
 from models.mediaonly_channel import MediaOnlyChannel
+from translations import translations
 from util import check_access
 
 WASTEBASKET = b"\xf0\x9f\x97\x91\xef\xb8\x8f".decode()
 
 
 def make_embed(requested_by: Member) -> Embed:
-    embed = Embed(title="Metafragen", url="http://metafrage.de/")
-    embed.description = (
-        "Eine Metafrage ist eine Frage über eine Frage, wie beispielsweise „Darf ich etwas fragen?“ "
-        "oder „Kennt sich jemand mit Computern aus?“.\n\nIn der Regel wird der Begriff Metafrage "
-        "aber verallgemeinert und damit alle Fragen bezeichnet, die keine direkte Frage zum Problem "
-        "des Hilfesuchenden sind. Der Hilfesuchende fragt also zunächst allgemein, ob jemand helfen "
-        "kann. Gerade Neulinge oder unerfahrene Benutzer lassen sich zu Metafragen hinreißen, um "
-        "einen kompetenten und hilfsbereiten Ansprechpartner zu finden. Meistens werden Metafragen "
-        "ignoriert oder der Fragende wird rüde darauf hingewiesen, dass ihm niemand bei seinem Problem "
-        "helfen könne, ohne dies zu kennen. Grundsätzlich folgt auf eine Meta-Frage eine weitere Frage."
-    )
-    embed.set_footer(text=f"Requested by @{requested_by} ({requested_by.id})", icon_url=requested_by.avatar_url)
+    embed = Embed(title=translations.metaquestion_title, url="http://metafrage.de/")
+    embed.description = translations.metaquestion_description
+    embed.set_footer(text=translations.f_requested_by(requested_by, requested_by.id), icon_url=requested_by.avatar_url)
     embed.add_field(
-        name="Vorteile von Metafragen",
-        value=(
-            "- als höfliche Floskel um Aufmerksamkeit zu gewinnen\n"
-            "- Beginn einer zunächst einseitigen Konversation (Allgemeine Problemanalyse)"
-        ),
+        name=translations.mq_advantages_title,
+        value="\n".join("- " + e for e in translations.mq_advantages),
         inline=False,
     )
     embed.add_field(
-        name="Nachteile von Metafragen",
-        value=(
-            "- die Anwesenden könnten eventuell bei dem Problem helfen, obwohl sie (eventuell aus Bescheidenheit)"
-            " nicht von sich behaupten würden, mit dem Thema vertraut zu sein,\n"
-            "- oft ist die Metafrage falsch formuliert, z. B. wird gefragt „kennt sich jemand mit Kochen aus?“ "
-            "und er will nur wissen, ob Mangos essbar sind,\n"
-            "- Auch wenn jemand mit dem erfragten Thema vertraut ist, bedeutet dies nicht, dass er eine spezielle"
-            " Frage zu diesem beantworten kann – niemand ist allwissend,\n"
-            "- wenn keine Reaktion auf die Metafrage erfolgt, beläßt es der Fragende meist dabei. So kann eine"
-            " Antwort auf das Problem von später aufmerksam Werdenden nicht erfolgen.\n"
-        ),
+        name=translations.mq_disadvantages_title,
+        value="\n".join("- " + e for e in translations.mq_disadvantages),
         inline=False,
     )
     return embed
@@ -77,7 +57,8 @@ class MetaQuestionCog(Cog, name="Metafragen"):
             return False
         elif emoji.name == WASTEBASKET:
             for embed in message.embeds:
-                if (match := re.match(r"^Requested by @.*?#\d{4} \((\d+)\)$", embed.footer.text)) is not None:
+                pattern = re.escape(translations.requested_by).replace("\\{\\}", "{}").format(r".*?#\d{4}", r"(\d+)")
+                if (match := re.match("^" + pattern + "$", embed.footer.text)) is not None:
                     author_id = int(match.group(1))
                     if not (author_id == member.id or await check_access(member)):
                         await message.remove_reaction(emoji, member)
