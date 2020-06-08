ffa11987d1d282d151450a3e14cf9f9bb68d454a
Defelo <elodef42@gmail.com>
2020-06-08 23:14:16 +0200

    incremented version number

--- info.py
+++ info.py
@@ -1,4 +1,4 @@
-VERSION = "1.2.0"
+VERSION = "1.3.0"
 MORPHEUS_ICON = "https://cdn.discordapp.com/avatars/686299664726622258/cb99c816286bdd1d988ec16d8ae85e15.png"
 CONTRIBUTORS = [212866839083089921, 330148908531580928]
 GITHUB_LINK = "https://github.com/Defelo/MorpheusHelper"
