2809c55a87595c7314f6c85d881e2b1b21bde9ba
ce_phox <paul-stier@gmx.de>
2020-07-12 14:53:53 +0200

    Moved help-command to HelpCog; Added subcommand help

--- morpheushelper.py
+++ morpheushelper.py
@@ -41,6 +41,7 @@ from cogs.reactionrole import ReactionRoleCog
 from cogs.rules import RulesCog
 from cogs.voice_channel import VoiceChannelCog
 from cogs.reddit import RedditCog
+from cogs.help import HelpCog
 from database import db
 from info import MORPHEUS_ICON, CONTRIBUTORS, GITHUB_LINK, VERSION
 from permission import Permission
@@ -118,46 +119,6 @@ async def status_loop():
             await owner.send(content)
         except Forbidden:
             pass
-
-
-@bot.command()
-async def help(ctx: Context, *args):
-    """
-    Shows this message
-    """
-
-    if len(args) == 0:
-        embed = Embed(title="Command Help", color=0x008080)
-        for cog in bot.cogs:
-            cog = bot.get_cog(cog)
-            title = cog.qualified_name
-            description = ""
-            for command in cog.get_commands():
-                description += command.name + " | " + command.short_doc + "\n"
-            embed.add_field(name=title, value=description, inline=False)
-
-        title = "No Category"
-        description = ""
-
-        for command in bot.commands:
-            if command.cog is None:
-                description += command.name + " | " + command.short_doc + "\n"
-
-        embed.add_field(name=title, value=description, inline=False)
-
-        embed.add_field(name="** **",
-                        value="Type {}help command for more info on a command.".format(bot.command_prefix),
-                        inline=False)
-
-        await ctx.send(embed=embed)
-
-    elif len(args) == 1:
-        command = bot.get_command(args[0])
-        executing = bot.command_prefix + "[" + command.name + ("|" if len(command.aliases) != 0 else "") + "|".join(
-            command.aliases) + "]"
-        embed = Embed(title="Command Help for " + args[0], description=executing, color=0x008080)
-        embed.add_field(name="Description", value=command.short_doc)
-        await ctx.send(embed=embed)
 
 
 @bot.command()
@@ -422,5 +383,6 @@ register_cogs(
     ModCog,
     PermissionsCog,
     RedditCog,
+    HelpCog
 )
 bot.run(os.environ["TOKEN"])
