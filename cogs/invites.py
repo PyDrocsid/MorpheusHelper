9015d5a59ca8d1c8e9da822b367161e977d99886
Florian Sabonchi <sabonchi@posteo.de>
2020-08-11 21:30:23 +0200

    Filtering for discord invites significantly improved (fixes #64)

--- cogs/invites.py
+++ cogs/invites.py
@@ -40,8 +40,8 @@ class AllowedServerConverter(Converter):
 
 def get_discord_invite(url) -> Optional[str]:
     while True:
-        if match := re.match(r"^(https?://)?discord(.gg|(app)?.com/invite)/([a-zA-Z0-9\-_]+)$", url):
-            return match.group(4)
+        if match := re.match(r'[A-Za-z]*[https(:)//]*discord.gg[.]*/([a-zA-Z0-9]+)', url, re.IGNORECASE):
+            return match.group(1)
 
         if not re.match(r"^(https?://).*$", url):
             url = "https://" + url
@@ -69,7 +69,7 @@ class InvitesCog(Cog, name="Allowed Discord Invites"):
 
         forbidden = []
         legal_invite = False
-        for url, *_ in re.findall(r"((https?://)?([a-zA-Z0-9\-_~]+\.)+[a-zA-Z0-9\-_~]+(/\S*)?)", message.content):
+        for url, *_ in re.findall(r"((https?://)?([a-zA-Z0-9\-_~]+\.)+[a-zA-Z0-9\-_~.]+(/\S*)?)", message.content):
             if (code := get_discord_invite(url)) is None:
                 continue
             try:
