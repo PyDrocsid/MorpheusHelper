90e0e7e32bbecb5462f2e593b66a3a10330dac3f
luwol03 <luwol03@gmail.com>
2020-08-18 13:37:53 +0200

    added embeds to news commands

--- cogs/news.py
+++ cogs/news.py
@@ -1,7 +1,7 @@
-import io
-from typing import Optional
+import re
+from typing import Optional, Union
 
-from discord import Member, TextChannel, Role, Guild, File, HTTPException, Forbidden
+from discord import Member, TextChannel, Role, Guild, HTTPException, Forbidden, Embed, Color
 from discord.ext import commands
 from discord.ext.commands import Cog, Bot, guild_only, Context, CommandError, UserInputError
 
@@ -9,7 +9,14 @@ from database import run_in_thread, db
 from models.news_authorization import NewsAuthorization
 from permission import Permission
 from translations import translations
-from util import permission_level, send_to_changelog, read_normal_message
+from util import (
+    permission_level,
+    send_to_changelog,
+    read_normal_message,
+    get_colour,
+    send_long_embed,
+    attachment_to_file,
+)
 
 
 class NewsCog(Cog, name="News"):
@@ -50,18 +57,21 @@ class NewsCog(Cog, name="News"):
             if text_channel is None or member is None:
                 await run_in_thread(db.delete, authorization)
                 continue
-            line = f"- `@{member}` -> {text_channel.mention}"
+            line = f":small_orange_diamond: {member.mention} -> {text_channel.mention}"
             if authorization.notification_role_id is not None:
                 role: Optional[Role] = guild.get_role(authorization.notification_role_id)
                 if role is None:
                     await run_in_thread(db.delete, authorization)
                     continue
-                line += f" (`@{role}`)"
+                line += f" ({role.mention})"
             out.append(line)
+        embed = Embed(title=translations.news, colour=get_colour(self))
         if out:
-            await ctx.send("\n".join(out))
+            embed.description = "\n".join(out)
         else:
-            await ctx.send(translations.no_news_authorizations)
+            embed.colour = get_colour("red")
+            embed.description = translations.no_news_authorizations
+        await send_long_embed(ctx, embed)
 
     @auth.command(name="add", aliases=["a", "+"])
     async def auth_add(self, ctx: Context, user: Member, channel: TextChannel, notification_role: Optional[Role]):
@@ -77,7 +87,8 @@ class NewsCog(Cog, name="News"):
         role_id = notification_role.id if notification_role is not None else None
 
         await run_in_thread(NewsAuthorization.create, user.id, channel.id, role_id)
-        await ctx.send(translations.news_authorized)
+        embed = Embed(title=translations.news, colour=get_colour(self), description=translations.news_authorized)
+        await ctx.send(embed=embed)
         await send_to_changelog(ctx.guild, translations.f_log_news_authorized(user.mention, channel.mention))
 
     @auth.command(name="remove", aliases=["del", "r", "d", "-"])
@@ -93,11 +104,13 @@ class NewsCog(Cog, name="News"):
             raise CommandError(translations.news_not_authorized)
 
         await run_in_thread(db.delete, authorization)
-        await ctx.send(translations.news_unauthorized)
+        embed = Embed(title=translations.news, colour=get_colour(self), description=translations.news_unauthorized)
+        await ctx.send(embed=embed)
         await send_to_changelog(ctx.guild, translations.f_log_news_unauthorized(user.mention, channel.mention))
 
     @news.command(name="send", aliases=["s"])
-    async def send(self, ctx: Context, channel: TextChannel, *, message: Optional[str]):
+    async def send(self, ctx: Context, channel: TextChannel, color: Optional[Union[Color, str]] = None, *,
+                   message: Optional[str]):
         """
         send a news message
         """
@@ -108,27 +121,41 @@ class NewsCog(Cog, name="News"):
         if authorization is None:
             raise CommandError(translations.news_you_are_not_authorized)
 
+        if isinstance(color, str):
+            if not re.match(r"^[0-9a-fA-F]{6}$", color):
+                raise CommandError(translations.invalid_color)
+            color = int(color, 16)
+
         if message is None:
             message = ""
 
+        embed = Embed(title=translations.news, colour=get_colour(self), description="")
         if not message and not ctx.message.attachments:
-            await ctx.send(translations.send_message)
+            embed.description = translations.send_message
+            await ctx.send(embed=embed)
             message, files = await read_normal_message(self.bot, ctx.channel, ctx.author)
         else:
-            files = []
-            for attachment in ctx.message.attachments:
-                file = io.BytesIO()
-                await attachment.save(file)
-                files.append(File(file, filename=attachment.filename, spoiler=attachment.is_spoiler()))
+            files = [await attachment_to_file(attachment) for attachment in ctx.message.attachments]
+
+        content = ""
+        send_embed = Embed(title=translations.news, description=message, colour=get_colour(self))
+        send_embed.set_footer(text=translations.f_sent_by(ctx.author, ctx.author.id), icon_url=ctx.author.avatar_url)
 
         if authorization.notification_role_id is not None:
             role: Optional[Role] = ctx.guild.get_role(authorization.notification_role_id)
             if role is not None:
-                message = role.mention + " " + message
+                content = role.mention
+
+        if color is not None:
+            send_embed.colour = color
+
+        if files:
+            send_embed.set_image(url="attachment://" + files[0].filename)
 
         try:
-            await channel.send(content=message, files=files)
+            await channel.send(content=content, embed=send_embed, files=files)
         except (HTTPException, Forbidden):
             raise CommandError(translations.msg_could_not_be_sent)
         else:
-            await ctx.send(translations.msg_sent)
+            embed.description = translations.msg_sent
+            await ctx.send(embed=embed)
