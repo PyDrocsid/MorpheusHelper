5bd0d6e795de62782c5eaa52e79187225463a943
Defelo <elodef42@gmail.com>
2020-09-12 23:16:44 +0200

    Improved code style

--- cogs/polls.py
+++ cogs/polls.py
@@ -1,49 +1,48 @@
 import re
+import string
 from datetime import datetime
 from typing import Optional
 
-import discord
-from PyDrocsid.emojis import name_to_emoji
+from PyDrocsid.emojis import name_to_emoji, emoji_to_name
 from PyDrocsid.translations import translations
 from discord import Embed, Message
 from discord.ext import commands
-from discord.ext.commands import Cog, Bot, Context, guild_only, UserInputError, CommandError
+from discord.ext.commands import Cog, Bot, Context, guild_only, CommandError
 
 MAX_OPTIONS = 20  # Discord reactions limit
 
-default_emojis = [name_to_emoji[f"regional_indicator_{x}"] for x in "abcdefghijklmnopqrstuvwyz"]
+default_emojis = [name_to_emoji[f"regional_indicator_{x}"] for x in string.ascii_lowercase]
 
 
 class PollsCog(Cog, name="Polls"):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command(usage="<question>\n[emoji1] <option1>\n[emojiX] [optionX]", aliases=["vote"])
+    @commands.command(usage=translations.poll_usage, aliases=["vote"])
     @guild_only()
-    async def poll(self, ctx: Context):
+    async def poll(self, ctx: Context, *, args: str):
         """
         Starts a poll. Multiline options can be specified using a `\\` at the end of a line
         """
-        lines = [line.replace("\x00", "\n") for line in ctx.message.content.replace("\\\n", "\x00").split("\n")]
 
-        if len(lines) < 2:
-            raise UserInputError("Missing options")
-        if len(lines) > MAX_OPTIONS + 1:
+        question, *options = [line.replace("\x00", "\n") for line in args.replace("\\\n", "\x00").split("\n")]
+
+        if not options:
+            raise CommandError(translations.missing_options)
+        if len(options) > MAX_OPTIONS:
             raise CommandError(translations.f_too_many_options(MAX_OPTIONS))
 
-        question = lines[0][len(ctx.prefix + ctx.invoked_with) + 1:]
-        options = [PollOption(ctx, line, i) for i, line in enumerate(lines[1:])]
+        options = [PollOption(ctx, line, i) for i, line in enumerate(options)]
 
-        embed = Embed(title=question,
-                      description=translations.vote_explanation,
-                      color=0xff1010,
-                      timestamp=datetime.utcnow())
-        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed = Embed(
+            title=question, description=translations.vote_explanation, color=0xFF1010, timestamp=datetime.utcnow()
+        )
+        embed.set_author(name=str(ctx.author), icon_url=ctx.author.avatar_url)
 
         for option in options:
             embed.add_field(name="** **", value=str(option), inline=False)
 
-        poll: discord.Message = await ctx.send(embed=embed)
+        poll: Message = await ctx.send(embed=embed)
 
         for option in options:
             await poll.add_reaction(option.emoji)
@@ -65,18 +64,19 @@ class PollsCog(Cog, name="Polls"):
 
 class PollOption:
     def __init__(self, ctx: Context, line: str, number: int):
-        if len(line) == 0:
-            raise UserInputError("Empty option")
+        if not line:
+            raise CommandError(translations.empty_option)
 
-        emoji_candidate = line.split(" ")[0]
+        emoji_candidate, *text = line.split(" ")
+        text = " ".join(text)
 
-        custom_emoji_match = re.fullmatch(r"<a?:([a-zA-Z0-9_])+:([0-9]+)>", emoji_candidate)
-        if custom_emoji := ctx.bot.get_emoji(int(custom_emoji_match.group(2))) if custom_emoji_match else None:
+        custom_emoji_match = re.fullmatch(r"<a?:[a-zA-Z0-9_]+:(\d+)>", emoji_candidate)
+        if custom_emoji := ctx.bot.get_emoji(int(custom_emoji_match.group(1))) if custom_emoji_match else None:
             self.emoji = custom_emoji
-            self.option = line[custom_emoji_match.end():].strip()
-        elif (unicode_emoji := emoji_candidate) in name_to_emoji.values():
+            self.option = text.strip()
+        elif (unicode_emoji := emoji_candidate) in emoji_to_name:
             self.emoji = unicode_emoji
-            self.option = line[len(unicode_emoji):].strip()
+            self.option = text.strip()
         else:
             self.emoji = default_emojis[number]
             self.option = line
