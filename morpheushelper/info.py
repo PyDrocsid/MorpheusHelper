d0ed5c97253fbbf4d570ad674b351035a94af252
Defelo <elodef42@gmail.com>
2020-10-22 17:20:49 +0200

    Improved project structure and Dockerfile

--- morpheushelper/info.py
+++ morpheushelper/info.py
@@ -1,6 +1,6 @@
 from subprocess import getoutput  # skipcq: BAN-B404
 
-VERSION = getoutput("git describe").lstrip("v")
+VERSION = getoutput("cat VERSION 2>/dev/null || git describe").lstrip("v")
 MORPHEUS_ICON = "https://cdn.discordapp.com/avatars/686299664726622258/cb99c816286bdd1d988ec16d8ae85e15.png"
 CONTRIBUTORS = [
     212866839083089921,
