81ef455a5b7731979a260501397b919b67864df2
Defelo <elodef42@gmail.com>
2020-11-05 20:31:26 +0100

    Reordered contributors

--- morpheushelper/info.py
+++ morpheushelper/info.py
@@ -5,13 +5,13 @@ import requests
 VERSION = getoutput("cat VERSION 2>/dev/null || git describe").lstrip("v")
 MORPHEUS_ICON = "https://cdn.discordapp.com/avatars/686299664726622258/cb99c816286bdd1d988ec16d8ae85e15.png"
 CONTRIBUTORS = [
-    330148908531580928,  # @AdriBloober#1260
-    137906177487929344,  # @Scriptim#3540
-    306774624090456075,  # @ce_phox#1259
-    541341790176018432,  # @Florian#1234
     212866839083089921,  # @TNT2k#7587
-    302365095688798209,  # @MaxiHuHe04#8905
     339062431131369472,  # @wolflu#5506
+    306774624090456075,  # @ce_phox#1259
+    137906177487929344,  # @Scriptim#3540
+    302365095688798209,  # @MaxiHuHe04#8905
+    541341790176018432,  # @Florian#1234
+    330148908531580928,  # @AdriBloober#1260
 ]
 GITHUB_LINK = "https://github.com/Defelo/MorpheusHelper"
 AVATAR_URL = "https://github.com/Defelo.png"
