47b511f57dded43cb3a4d45c67dfe490cdcbd490
FlorianJSa <sabonchi@posteo.de>
2021-02-05 21:02:07 +0100

    Added remove_markdown

--- morpheushelper/cogs/run_code.py
+++ morpheushelper/cogs/run_code.py
@@ -1,3 +1,5 @@
+import re
+
 from PyDrocsid.translations import translations
 from PyDrocsid.util import send_long_embed
 from discord import Embed
@@ -12,6 +14,13 @@ import json
 from emkc_api import Emekc
 
 
+def remove_markdown(source: str):
+    match = re.match(r'(^.*`{3,}\w*)(.+)(`{3,}.*)$', source,  re.DOTALL)
+    if "\n" not in source:
+        return source.replace("`", "")
+    return match.group(2)
+
+
 class RunCodeCog(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
@@ -19,13 +28,7 @@ class RunCodeCog(Cog):
     @commands.command(name="run")
     async def run(self, ctx, language: str, *, source: str):
         try:
-            markdown = ["awk", "bash", "brainfuck", "c", "cpp", "crystal",
-                        "csharp", "d", "go", "haskell", "java", "js" "julia", "kotlin", "lisp", "lua",
-                        "nim", "perl", "php", "py", "python", "ruby", "rust", "swift", "typescript", "`"]
-
-            for item in markdown:
-                source = source.replace(item, "")
-            api_result = json.loads(await Emekc.run_code({'language': language, 'source': source}))
+            api_result = json.loads(await Emekc.run_code({'language': language, 'source': remove_markdown(source)}))
             if "output" not in api_result:
                 raise CommandError(translations.f_error_unsupported_language(language))
             await send_long_embed(ctx, Embed(colour=Colours.green, description=api_result["output"]))
