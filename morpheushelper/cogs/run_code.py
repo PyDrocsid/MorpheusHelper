c1f3d3a621bca34f7e5d33faed611a900fe61fb2
Defelo <elodef42@gmail.com>
2021-02-10 00:11:36 +0100

    Improved i run code feature

--- morpheushelper/cogs/run_code.py
+++ morpheushelper/cogs/run_code.py
@@ -4,33 +4,66 @@ from PyDrocsid.translations import translations
 from PyDrocsid.util import send_long_embed
 from discord import Embed
 from discord.ext import commands
-from discord.ext.commands import Cog, Bot, CommandError
+from discord.ext.commands import Cog, Bot, CommandError, UserInputError
 from requests import RequestException
 
 from colours import Colours
+from emkc_api import Emkc, EmkcAPIException
 
-import json
+N_CHARS = 1000
 
-from emkc_api import Emekc
+LANGUAGES = (
+    "awk,bash,brainfuck,c,cpp,crystal,csharp,d,dash,deno,elixir,emacs,go,haskell,java,jelly,julia,"
+    "kotlin,lisp,lua,nasm,nasm64,nim,node,osabie,paradoc,perl,php,python2,python3,ruby,rust,swift,"
+    "typescript,zig"
+).split(",")
 
 
-def remove_markdown(source: str):
-    match = re.match(r'(^.*`{3,}\w*)(.+)(`{3,}.*)$', source,  re.DOTALL)
-    if "\n" not in source:
-        return source.replace("`", "")
-    return match.group(2)
+def supported_languages_docs(f):
+    f.__doc__ += f"\nSupported languages: {', '.join(f'`{lang}`' for lang in LANGUAGES)}"
+    return f
 
 
 class RunCodeCog(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command(name="run")
-    async def run(self, ctx, language: str, *, source: str):
+    @commands.command(usage=translations.run_usage)
+    @supported_languages_docs
+    async def run(self, ctx, *, args: str):
+        """
+        run some code
+        """
+
+        if not (match := re.fullmatch(r"```([a-zA-Z\d]+)\n(.+?)```", args, re.DOTALL)):
+            raise UserInputError
+
+        language, source = match.groups()
+
+        await ctx.trigger_typing()
+
         try:
-            api_result = json.loads(await Emekc.run_code({'language': language, 'source': remove_markdown(source)}))
-            if "output" not in api_result:
+            api_result: dict = await Emkc.run_code(language, source)
+        except EmkcAPIException as e:
+            if e.message == "Supplied language is not supported by Piston":
                 raise CommandError(translations.f_error_unsupported_language(language))
-            await send_long_embed(ctx, Embed(colour=Colours.green, description=api_result["output"]))
+            raise CommandError(f"{translations.error_run_code}: {e.message}")
         except RequestException:
             raise CommandError(translations.error_run_code)
+
+        output: str = api_result["output"]
+        if len(output) > N_CHARS:
+            newline = output.find("\n", N_CHARS, N_CHARS + 20)
+            if newline == -1:
+                newline = N_CHARS
+            output = output[:newline] + "\n..."
+
+        description = "```\n" + output.replace("`", "`\u200b") + "\n```"
+
+        embed = Embed(title=translations.run_output, color=Colours.green, description=description)
+        if api_result["stderr"] and not api_result["stdout"]:
+            embed.colour = Colours.error
+
+        embed.set_footer(text=translations.f_requested_by(ctx.author, ctx.author.id), icon_url=ctx.author.avatar_url)
+
+        await send_long_embed(ctx, embed)
--- morpheushelper/emkc_api.py
+++ morpheushelper/emkc_api.py
@@ -1,11 +1,20 @@
 from aiohttp import ClientSession
 
 
-class Emekc:
+class EmkcAPIException(BaseException):
+    @property
+    def message(self) -> str:
+        return self.args[0]["message"]
+
+
+class Emkc:
     URL = "https://emkc.org/api/v1/piston/execute"
 
     @staticmethod
-    async def run_code(data: dict):
+    async def run_code(language: str, source: str) -> dict:
         async with ClientSession() as session:
-            async with session.post(Emekc.URL, data=data) as response:
-                return await response.text()
+            async with session.post(Emkc.URL, json={"language": language, "source": source}) as response:
+                if response.status != 200:
+                    raise EmkcAPIException(await response.json())
+
+                return await response.json()
