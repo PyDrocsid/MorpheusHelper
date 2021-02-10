20d5af02a17b140fed5280d790670f2a7e017bb2
Houseplant1 <nsap@tuta.io>
2021-02-10 10:26:30 +0100

    better error handling, removed random colors

--- morpheushelper/cogs/wikipedia.py
+++ morpheushelper/cogs/wikipedia.py
@@ -1,14 +1,13 @@
-from discord.member import Member
 from discord.embeds import Embed
 from discord.ext import commands
 from discord.ext.commands import Cog, Bot
 from discord.ext.commands.context import Context
-from discord.colour import Color
+from colours import Colours
 
 import wikipedia
 
-def make_embed(title: str, content: str) -> Embed:
-    embed = Embed(title=title, description=content, color=Color.random())
+def make_embed(title: str, content: str, color) -> Embed:
+    embed = Embed(title=title, description=content, color=color)
     return embed
 
 # Note: wikiepdia allows bots, but only bots that are responsible (not going too fast).
@@ -19,4 +18,11 @@ class WikipediaCog(Cog, name="Wikipedia"):
     @commands.command(aliases=["wiki", "summary", "wk", "sum"])
     @commands.cooldown(5, 10, commands.BucketType.user)
     async def wikipedia(self, ctx: Context, *, title: str):
-        await ctx.send(embed=make_embed(title=title, content=wikipedia.summary(title)))
+        """
+            display wikipedia summary about a topic 
+        """
+        try:
+            await ctx.send(embed=make_embed(title=title, content=wikipedia.summary(title), color=Colours.default))
+
+        except Exception as e:
+            await ctx.send(embed=make_embed(title="Not found!", content=str(e), color=Colours.warning))
