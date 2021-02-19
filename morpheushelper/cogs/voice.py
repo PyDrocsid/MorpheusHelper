24f6a34825e14ab8f0d52743ac3ba532d97c5ec6
m <markusscoding@outlook.com>
2021-02-19 18:48:32 +0100

    Check if person has the InVoice role

--- morpheushelper/cogs/voice.py
+++ morpheushelper/cogs/voice.py
@@ -24,30 +24,34 @@ class VoicePlus(discord.Client):
         self.users.append(user_dict)
 
     @tasks.loop(minutes=1)
-    async def check_dict():
+    async def check_dict(self):
         for item in self.users:
             if item["timemuted"].minute < 60:
                 if item["timemuted"].minute + 5 == datetime.datetime.now().minute:
-                    users.remove(item)
+                    self.users.remove(item)
             else:
                 item["timemuted"].minute = item["timemuted"].minute - 60
 
     @bot.command(name="vmute", aliases=['vm', 'm'])
     async def vmute(self, ctx, member: discord.Member, duration=0, *, unit=None):
-        if member in self.users["user"]:
-            member.mute_members = True
+        role = discord.utils.find(
+            lambda r: r.name == 'InVoice', ctx.message.server.roles)
+        if user.has_role(role):
+            await member.edit(mute=True)
+        elif member in self.users["user"]:
+            await member.edit(mute=True)
             if unit == "s":
                 wait = 1 * duration
                 await asyncio.sleep(wait)
             elif unit == "m":
                 wait = 60 * duration
                 await asyncio.sleep(wait)
-            member.mute_members = False
+            await member.edit(mute=False)
 
     @bot.command(name="vunmute", aliases=['vu', 'u'])
     async def unmute(self, member: discord.Member):
-        await member.mute_members = False
+        await member.edit(mute=False)
 
     @bot.command(name="vkick", aliases=['vk', 'k'])
     async def vkick(self, member: discord.Member):
-        pass
+        await member.move_to(None)
