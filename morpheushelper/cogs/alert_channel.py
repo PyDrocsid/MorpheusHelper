4ee36c4089e4cae6872cc4d8f795b758683781b4
cephox <paul-stier@gmx.de>
2021-02-24 13:47:00 +0100

    Moved alert channel to settings table in db

--- morpheushelper/cogs/alert_channel.py
+++ morpheushelper/cogs/alert_channel.py
@@ -1,7 +1,6 @@
 import logging
 from typing import Optional
 
-from PyDrocsid.database import db, db_thread
 from PyDrocsid.settings import Settings
 from PyDrocsid.translations import translations
 from discord import TextChannel, Embed, Guild, Member, VoiceState
@@ -10,7 +9,6 @@ from discord.ext.commands import Cog, Bot, Context
 from discord.utils import get
 
 from colours import Colours
-from models.alert_channel import AlertChannel
 from permissions import PermissionLevel
 
 
@@ -18,13 +16,13 @@ async def getAlertChannel(guild: Guild) -> Optional[TextChannel]:
     """
     Retrieves the alert-channel of the specified guild
     """
-    alert_channel: AlertChannel = await db_thread(db.get, AlertChannel, guild.id)
-    if not alert_channel:
+    alert_channel_id: int = await Settings.get(int, key="alert_channel", default=-1)
+    if alert_channel_id <= 0:
         return None
 
-    text_channel: TextChannel = get(guild.text_channels, id=alert_channel.channel)
+    text_channel: TextChannel = get(guild.text_channels, id=alert_channel_id)
     if not text_channel:
-        db.delete(alert_channel)
+        await Settings.set(int, "alert_channel", -1)
 
     return text_channel
 
@@ -33,7 +31,7 @@ async def getMaxHops() -> int:
     """
     Retrieves the channel hops per minute in order for a message to appear
     """
-    return await Settings.get(int, "alert_channel_warn_channel_hops", 5)
+    return await Settings.get(int, key="alert_channel_warn_channel_hops", default=5)
 
 
 class AlertChannelCog(Cog):
@@ -57,7 +55,7 @@ class AlertChannelCog(Cog):
             del self.user_hops[member.id]
             embed = Embed(title=translations.alert_channel_hop, color=Colours.AlertChannel)
             embed.add_field(name=translations.member, value=member.mention)
-            embed.add_field(name=translations.alert_channel_hop_current_channel, value=after.channel.mention)
+            embed.add_field(name=translations.alert_channel_hop_current_channel, value=after.channel.name)
             if ch := await getAlertChannel(member.guild):
                 await ch.send(embed=embed)
             else:
@@ -76,9 +74,15 @@ class AlertChannelCog(Cog):
         """
         if not ctx.subcommand_passed:
             embed = Embed(title=translations.alert_channel, colour=Colours.AlertChannel)
-            embed.add_field(name=translations.alert_channel_get,
-                            value=(await getAlertChannel(ctx.guild)).mention,
-                            inline=False)
+            channel: TextChannel = await getAlertChannel(ctx.guild)
+            if channel:
+                embed.add_field(name=translations.alert_channel_get,
+                                value=channel.mention,
+                                inline=False)
+            else:
+                embed.add_field(name=translations.alert_channel_get,
+                                value=translations.none,
+                                inline=False)
             embed.add_field(name=translations.alert_channel_hop_current_amount, value=str(await getMaxHops()),
                             inline=False)
             await ctx.send(embed=embed)
@@ -88,7 +92,7 @@ class AlertChannelCog(Cog):
         """
         Updated the alert channel
         """
-        await db_thread(AlertChannel.create, ctx.guild.id, channel.id)
+        await Settings.set(int, "alert_channel", channel.id)
 
         embed = Embed(title=translations.alert_channel, description=translations.alert_channel_set,
                       color=Colours.AlertChannel)
