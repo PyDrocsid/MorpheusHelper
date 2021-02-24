68062d0d96e1d1224798ae96f2a716eb4fb6d2de
use-to <use-to@outlook.de>
2021-02-24 22:09:36 +0100

    fixed redis implementation

--- morpheushelper/warn/user_warn_level.py
+++ morpheushelper/warn/user_warn_level.py
@@ -2,7 +2,17 @@ from aioredis import create_redis_pool
 from config.config import get_config_entry
 from discord import Member
 
-redis = await create_redis_pool(get_config_entry("REDIS_HOSTNAME") + ":" + get_config_entry("REDIS_PORT"))
+redis = None
+
+
+async def get_redis():
+    global redis
+
+    if redis:
+        return
+
+    redis = await create_redis_pool(
+        "redis://" + get_config_entry("REDIS_HOSTNAME") + ":" + get_config_entry("REDIS_PORT"))
 
 
 def _get_redis_key(member: Member) -> str:
@@ -10,6 +20,8 @@ def _get_redis_key(member: Member) -> str:
 
 
 async def get_user_warn_score(member: Member) -> int:
+    await get_redis()
+
     warn_score: int = await redis.get(_get_redis_key(member))
 
     if not warn_score:
@@ -19,6 +31,8 @@ async def get_user_warn_score(member: Member) -> int:
 
 
 async def increase_warn_score(member: Member) -> int:
+    await get_redis()
+
     warn_score: int = await get_user_warn_score(member) + 1
 
     await redis.set(_get_redis_key(member), warn_score, ex=2 * 60 * 60)  # 2 hour expiration time
