cdb4afedafe21c1575d29fd9cda8038ee5afa580
LoC <locrealloc@gmail.com>
2021-02-28 12:35:30 +0100

    Fixed typing
    Signed-off-by: LoC <locrealloc@gmail.com>

--- morpheushelper/cogs/rules.py
+++ morpheushelper/cogs/rules.py
@@ -17,8 +17,8 @@ class RulesCog(Cog, name="Rule Commands"):
         self.bot = bot
 
     @staticmethod
-    async def get_message_cancel(bot: Bot, channel: TextChannel, member: Member) -> [Tuple[Optional[str],
-                                                                                           List[Optional[File]]]]:
+    async def get_message_cancel(bot: Bot, channel: TextChannel, member: Member) -> tuple[Optional[str],
+                                                                                           list[Optional[File]]]:
         content, files = await read_normal_message(bot, channel, member)
         if content == translations.cancel:
             embed = Embed(title=translations.rule, colour=Colours.RuleCommands,
