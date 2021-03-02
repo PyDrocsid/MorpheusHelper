dcc9fa8c0c65ca632a47dc8231a287bfe80d2d09
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-03-02 21:45:07 +0100

    Removed old Todo and renamed var

--- morpheushelper/cogs/color_picker.py
+++ morpheushelper/cogs/color_picker.py
@@ -16,10 +16,10 @@ class ColorPickerCog(Cog):
         if not (color_code_re := re.match(r'^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$', color_code)):
             embed: Embed = Embed(title=translations.f_error_parse_color(color_code),
                                  description=translations.error_parse_color_example)
-            await ctx.send(embed=embed)  # TODO
+            await ctx.send(embed=embed)
             return
         rgb: tuple[int] = ImageColor.getcolor(color_code, "RGB")
-        hsl: tuple[int] = ImageColor.getcolor(color_code, "HSV")
+        hsv: tuple[int] = ImageColor.getcolor(color_code, "HSV")
         img: Image = Image.new('RGB', (100, 100), rgb)
         with io.BytesIO() as image_binary:
             img.save(image_binary, 'PNG')
@@ -27,6 +27,6 @@ class ColorPickerCog(Cog):
             embed: Embed = Embed(title='Colorpicker', color=Colour(int(color_code_re.group(1), 16)))
             embed.add_field(name='HEX', value=color_code)
             embed.add_field(name='RGB', value=str(rgb))
-            embed.add_field(name='HSV', value=str(hsl))
+            embed.add_field(name='HSV', value=str(hsv))
             embed.set_image(url="attachment://color.png")
             await ctx.send(embed=embed, file=File(fp=image_binary, filename='color.png'))
