cec1fe7e3416e4838f6a663091e56a4cf31044b2
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-03-03 19:29:25 +0100

    Compiles Regex and improved convertions

--- morpheushelper/cogs/color_picker.py
+++ morpheushelper/cogs/color_picker.py
@@ -9,36 +9,35 @@ import colorsys
 
 
 class ColorPickerCog(Cog):
+    RE_HEX = re.compile(r'^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$')
+    RE_RGB = re.compile(r'^rgb\(([0-9]{1,3})\, ?([0-9]{1,3})\, ?([0-9]{1,3})\)$')
+    RE_HSV = re.compile(r'^hsv\(([0-9]{1,3})\, ?([0-9]{1,3})\, ?([0-9]{1,3})\)$')
+    RE_HSL = re.compile(r'^hsl\(([0-9]{1,3})\, ?([0-9]{1,3})\, ?([0-9]{1,3})\)$')
+
     def __init__(self, bot: Bot):
         self.bot = bot
 
     @commands.command(name="colorpicker", aliases=["cp", "color"])
     async def colorpicker(self, ctx, *, color: str):
-        if color_re := re.match(r'^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$', color):
+        if color_re := self.RE_HEX.match(color):
             color_hex = color_re.group(1)
             rgb: tuple[int] = ImageColor.getcolor(color, "RGB")
             hsv: tuple[int] = ImageColor.getcolor(color, "HSV")
-            hsl = colorsys.rgb_to_hls(rgb[0], rgb[1], rgb[2])
-            hsl = (int(hsl[0]), int(hsl[1]), int(hsl[2]))
-        elif color_re := re.match(r'^rgb\(([0-9]{1,3})\, ?([0-9]{1,3})\, ?([0-9]{1,3})\)$', color):
+            hsl = map(int, colorsys.rgb_to_hls(*rgb))
+        elif color_re := self.RE_RGB.match(color):
             rgb = (int(color_re.group(1)), int(color_re.group(2)), int(color_re.group(3)))
             color_hex = '%02x%02x%02x' % rgb
             hsv: tuple[int] = ImageColor.getcolor('#' + color_hex, "HSV")
-            hsl = colorsys.rgb_to_hls(rgb[0], rgb[1], rgb[2])
-            hsl = (int(hsl[0]), int(hsl[1]), int(hsl[2]))
-        elif color_re := re.match(r'^hsv\(([0-9]{1,3})\, ?([0-9]{1,3})\, ?([0-9]{1,3})\)$', color):
+            hsl = map(int, colorsys.rgb_to_hls(*rgb))
+        elif color_re := self.RE_HSV.match(color):
             hsv: tuple[int] = (int(color_re.group(1)), int(color_re.group(2)), int(color_re.group(3)))
-            rgb = colorsys.hsv_to_rgb(hsv[0], hsv[1], hsv[2])
-            rgb = (int(rgb[0]), int(rgb[1]), int(rgb[2]))
+            rgb = map(int,  colorsys.hsv_to_rgb(*hsv))
             color_hex = '%02x%02x%02x' % rgb
-            hsl = colorsys.rgb_to_hls(rgb[0], rgb[1], rgb[2])
-            hsl = (int(hsl[0]), int(hsl[1]), int(hsl[2]))
-        elif color_re := re.match(r'^hsl\(([0-9]{1,3})\, ?([0-9]{1,3})\, ?([0-9]{1,3})\)$', color):
+            hsl = map(int, colorsys.rgb_to_hls(*rgb))
+        elif color_re := self.RE_HSL.match(color):
             hsl: tuple[int] = (int(color_re.group(1)), int(color_re.group(2)), int(color_re.group(3)))
-            rgb = colorsys.hls_to_rgb(hsl[0], hsl[1], hsl[2])
-            rgb = (int(rgb[0]), int(rgb[1]), int(rgb[2]))
-            hsv = colorsys.rgb_to_hsv(rgb[0], rgb[1], rgb[2])
-            hsv = (int(hsv[0]), int(hsv[1]), int(hsv[2]))
+            rgb = map(int,  colorsys.hls_to_rgb(*hsl))
+            hsl = map(int, colorsys.rgb_to_hsv(*rgb))
             color_hex = '%02x%02x%02x' % rgb
         else:
             embed: Embed = Embed(title=translations.f_error_parse_color(color),
