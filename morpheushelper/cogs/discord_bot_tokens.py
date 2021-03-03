8a93eff12fb70ee546c978698c125a9b509eccac
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-03-03 08:25:15 +0100

    added discord bot token cog with an token info and a token deleter

---
+++ morpheushelper/cogs/discord_bot_tokens.py
@@ -0,0 +1,45 @@
+from PyDrocsid.translations import translations
+from discord import Embed, Member, Message, Forbidden, NotFound, HTTPException
+from discord.ext import commands
+from discord.ext.commands import Cog, Bot, Context, guild_only
+
+from colours import Colours
+import re
+
+
+def make_embed(requested_by: Member) -> Embed:
+    embed = Embed(title=translations.discordbottoken_title, colour=Colours.DiscordBotToken)
+    embed.description = translations.discordbottoken_description
+    embed.set_footer(text=translations.f_requested_by(requested_by, requested_by.id), icon_url=requested_by.avatar_url)
+    return embed
+
+
+class DiscordBotTokenCog(Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @commands.command(aliases=["dctoken"])
+    @guild_only()
+    async def token(self, ctx: Context):
+        """
+        display information about discord bot toktens
+        """
+
+        await ctx.send(embed=make_embed(ctx.author))
+        try:
+            await ctx.message.delete()
+        except (Forbidden, NotFound, HTTPException):
+            pass
+
+    @guild_only()
+    async def on_message(self, msg: Message):
+        """
+        deletes a message if it contains a discord bot token
+        """
+
+        if re.match(r'[A-Za-z\d]{24}\.[A-Za-z\d]{6}\.[A-Za-z\d\-\_]{27}', msg.content):
+            await msg.channel.send(embed=make_embed(msg.author))
+            try:
+                await msg.delete()
+            except (Forbidden, NotFound, HTTPException):
+                pass
