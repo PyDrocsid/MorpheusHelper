8b7b6b86962acc0277faac32f4c2fce3b755880c
Florian Sabonchi <sabonchi@posteo.de>
2021-03-06 01:11:52 +0100

    Fixed DeepSource

--- morpheushelper/cogs/joined_info.py
+++ morpheushelper/cogs/joined_info.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from PyDrocsid.database import db_thread, db
 from PyDrocsid.translations import translations
-from discord import Member, Embed, member
+from discord import Member, Embed
 from discord.ext import commands
 from discord.ext.commands import Cog, Context, Bot, guild_only
 
@@ -37,11 +37,11 @@ class JoinedInfoCog(Cog):
         last_auto_kick = await db_thread(lambda: db.query(Kick).filter_by(member=user.id, mod=None)
                                          .order_by(Kick.timestamp.desc()).first())
         last_join_after_kick = await db_thread(lambda: db.query(Join).filter(Join.member == user.id)
-                                               .order_by(Join.timestamp.desc()).first())
+                                               .order_by(Join.timestamp.desc()).first().timestamp)
         if last_auto_kick:
             last_join_after_kick = await db_thread(
                 lambda: db.query(Join).filter(Join.timestamp > last_auto_kick.timestamp,
-                                              Join.member == user.id)
-                    .order_by(Join.timestamp.asc()).first().timestamp)
-        await ctx.send(embed=Embed(title=translations.joined_info, description=f"{user.mention}"
-                                                                               f" {date_diff_to_str(datetime.today(), last_join_after_kick)}"))
+                                              Join.member == user.id).order_by(Join.timestamp.asc()).first().timestamp)
+        embed = Embed(title=translations.joined_info,
+                      description=f"{user.mention} {date_diff_to_str(datetime.today(), last_join_after_kick)}")
+        await ctx.send(embed=embed)
