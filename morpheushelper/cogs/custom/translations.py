174d6d0af7ff3a01c615bc4ff2efd4127904bcc5
Defelo <elodef42@gmail.com>
2021-04-09 00:18:30 +0200

    Updated submodules

--- morpheushelper/cogs/custom/translations.py
+++ morpheushelper/cogs/custom/translations.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-from cogs.library.translations import visit_path
+from cogs.library.translations import load_translations
 
-visit_path(Path(__file__).parent, prio=1)
+load_translations(Path(__file__).parent, prio=1)
