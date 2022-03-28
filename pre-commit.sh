87a5104dadc8671b96ccc9d138c911f1bb94f9c3
Defelo <elodef42@gmail.com>
2022-03-28 23:06:27 +0200

    Added pre-commit.sh

---
+++ pre-commit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# install via ./pre-commit.sh install
+
+if [[ "$1" = "install" ]]; then
+    hooks=$(git rev-parse --git-path hooks)
+    ln -s $(realpath --relative-to $hooks pre-commit.sh) $hooks/pre-commit
+    exit 0
+fi
+
+tmp=$(mktemp)
+git diff > $tmp
+git apply --allow-empty -R $tmp
+
+$HOME/.local/bin/poe pre-commit
+code=$?
+
+git add -u
+git apply --allow-empty $tmp
+rm $tmp
+
+exit $code
