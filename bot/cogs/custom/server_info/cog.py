6bfa1ec022a9f0525b2d6bcde01740c9e9b93386
Defelo <elodef42@gmail.com>
2022-04-02 23:16:43 +0200

    Fixed import

--- bot/cogs/custom/server_info/cog.py
+++ bot/cogs/custom/server_info/cog.py
@@ -5,7 +5,7 @@ from discord import Guild, Member, Role
 from PyDrocsid.settings import RoleSettings
 from PyDrocsid.translations import t
 
-from cogs.library import ServerInfoCog
+from cogs.library.information import ServerInfoCog
 
 
 t = t.server_info
